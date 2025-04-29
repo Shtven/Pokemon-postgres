30048bea376c0573219907a3e0dda14f3b66a391
Shtven <gael1239983@gmail.com>
2025-04-29 02:04:38 -0600

    config2232

--- database/config/config.js
+++ database/config/config.js
@@ -1,24 +1,28 @@
-require('dotenv').config()
-const fs = require('fs');
+require('dotenv').config();
+
+const sslConfig = {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // A veces necesario si es un certificado autofirmado
+    },
+  },
+};
 
 module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
     dialect: 'postgres',
+    ...sslConfig
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
     dialect: 'postgres',
+    ...sslConfig
   },
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
+    ...sslConfig
+  },
 };
-
