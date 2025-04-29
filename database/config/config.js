b19302f0d0ae92e0258fea68d007060da5a20c0b
Shtven <gael1239983@gmail.com>
2025-04-28 23:47:35 -0600

    up config

--- database/config/config.js
+++ database/config/config.js
@@ -13,6 +13,12 @@ module.exports = {
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
-  },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 };
 
